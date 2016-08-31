3c6dd2fb0dba4bd23d8b286c3a54dd2ba751a44a
qinghuo <qinghuo@mogujie.com>
2016-08-31 10:04:18 +0800

    first

--- adapter/storm/src/test/java/com/huawei/streaming/storm/SystemPropertyBackupTest.java
+++ adapter/storm/src/test/java/com/huawei/streaming/storm/SystemPropertyBackupTest.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,27 +17,24 @@
  */
 package com.huawei.streaming.storm;
 
-import static org.junit.Assert.assertEquals;
+import com.google.common.base.Strings;
+import org.junit.Test;
 
 import java.util.Properties;
 
-import org.junit.Test;
-
-import com.google.common.base.Strings;
+import static org.junit.Assert.assertEquals;
 
 /**
  * <一句话功能简述>
  * <功能详细描述>
  *
  */
-public class SystemPropertyBackupTest
-{
+public class SystemPropertyBackupTest {
 
     private Properties backupSecurityConf = new Properties();
 
     @Test
-    public void proptertyTest1()
-    {
+    public void proptertyTest1() {
         SystemPropertyBackupTest tt = new SystemPropertyBackupTest();
         System.setProperty("dfa", "abc");
         tt.backupSecurityProperties("dfa");
@@ -65,26 +62,18 @@ public class SystemPropertyBackupTest
 
     }
 
-    private void backupSecurityProperties(String propertyKey)
-    {
-        if (System.getProperty(propertyKey) == null)
-        {
+    private void backupSecurityProperties(String propertyKey) {
+        if (System.getProperty(propertyKey) == null) {
             backupSecurityConf.put(propertyKey, "");
-        }
-        else
-        {
+        } else {
             backupSecurityConf.put(propertyKey, System.getProperty(propertyKey));
         }
     }
 
-    private void restoreSecurityProperties(String propertyKey)
-    {
-        if (Strings.isNullOrEmpty(backupSecurityConf.getProperty(propertyKey)))
-        {
+    private void restoreSecurityProperties(String propertyKey) {
+        if (Strings.isNullOrEmpty(backupSecurityConf.getProperty(propertyKey))) {
             System.clearProperty(propertyKey);
-        }
-        else
-        {
+        } else {
             System.setProperty(propertyKey, backupSecurityConf.getProperty(propertyKey));
         }
     }
